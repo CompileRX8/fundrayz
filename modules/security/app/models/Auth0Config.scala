314049501405de75329109c096348b501229b453
Ryan Highley <ryan@highley.net>
2016-09-05 14:33:48 -0500

    Updating for separate modules directory. Starting security module

---
+++ modules/security/app/models/Auth0Config.scala
@@ -0,0 +1,15 @@
+package models
+
+import javax.inject.Inject
+
+import play.api.Configuration
+
+/**
+  * Created by ryan on 4/3/16.
+  */
+class Auth0Config @Inject()(config: Configuration) {
+  def secret: String = { config.getString("auth0.clientSecret").get }
+  def clientId: String = { config.getString("auth0.clientId").get }
+  def callbackURL: String = { config.getString("auth0.callbackURL").get }
+  def domain: String = { config.getString("auth0.domain").get }
+}
